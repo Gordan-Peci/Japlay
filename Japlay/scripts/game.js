d5ebb01a6401a24dc63b8a53de4ea04049a2971f
Gordan-Peci <gordan.pecikozic@gmail.com>
2025-09-23 13:53:38 +0200

    Added title, score and a canvas. Title is an html h1 element that in js pull data from array levels in which there is 5 elements for 5 levels. later will change as player progresses the levels. Also created a score variable that starts at 0. Also score is an h3 element that pulls data from varibale score in javascript that will add score acordingly to players progress. #6

---
+++ Japlay/scripts/game.js
@@ -0,0 +1,5 @@
+const levels = ['Japlay N5', 'Japlay N4', 'Japlay N3', 'Japlay N2', 'Japlay N1'];
+document.getElementById('gameTitle').innerText = levels[0];
+
+const score = 0;
+document.getElementById('score').innerText = `Score: ${score}`;
